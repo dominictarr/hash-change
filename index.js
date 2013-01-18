755491fd5d418ac1b759933cb55835441748d01d
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-18 21:56:55 +0700

    add hash function

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var EventEmitter = require('events').EventEmitter
 var hashchange = module.exports = new EventEmitter()
 
 window.addEventListener('hashchange', function () {
-  var hash = window.location.hash.substring(1)
-  hashchange.emit('change', hash)
+  hashchange.emit('change', hashchange.hash())
 })
+
+hashchange.hash = function () {
+  return window.location.hash.substring(1)
+}
